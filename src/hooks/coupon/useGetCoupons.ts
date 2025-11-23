661175b70c13fb73003e48e7ff5818c457fa8a25
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-11-23 12:57:23 +0530

    Sunday2

--- src/hooks/coupon/useGetCoupons.ts
+++ src/hooks/coupon/useGetCoupons.ts
@@ -1,14 +1,13 @@
 import { Coupon } from "@/lib/types/couponType";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import apiClient from "@/lib/axios";
 
 export function useGetCoupons() {
   return useQuery<Coupon[]>({
     queryKey: ["coupons"],
     queryFn: async () => {
-      const response = await axios.get(
-        `api/coupon/list`,
-        { withCredentials: true }
+      const response = await apiClient.get(
+        `coupon/list`
       );
       if (!response?.data) throw new Error("Failed to fetch coupons");
       return response.data.coupons || [];
